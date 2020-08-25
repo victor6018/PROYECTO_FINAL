74b872657f87282eef1354b5406f8f5dc635b54a
victor6018 <josccolaura@gmail.com>
2020-08-24 21:50:26 -0500

    principal estudiante

---
+++ principal/estudiante/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="principal_estudiante">
+    <p>Bienvenido a nuestro sistema de gestion academica, en el que se incluye su información académica.</p>
+    <p>Este sistema está a su disposición, esperamos que le permita accdeder de manera rápida y eficiente a la informacion que busca.</p>
+    </div>
+    <div>
+        <div><a href="index.php">Inicio</a></div>
+        <div><a href="">Ver mi perfil </a></div>
+        <div><a href="">Reportes</a></div>
+        <div><a href="">Ver mis cursos</a></div>
+        <div><a href="">Matrícula</a></div>
+        <div><a href="">Horario</a></div>
+        <div><a href="">Cerrar sesión</a></div>
+        
+    </div>
+
+    
+</body>
+</html>
