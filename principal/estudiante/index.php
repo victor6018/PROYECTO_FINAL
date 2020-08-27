85a4d06245f391cef3f3f363dde55d415191dee4
victor6018 <josccolaura@gmail.com>
2020-08-26 22:21:09 -0500

    v3

--- principal/estudiante/index.php
+++ principal/estudiante/index.php
@@ -3,20 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/estiloinicio.css">
 </head>
 <body>
     <div class="principal_estudiante">
     <p>Bienvenido a nuestro sistema de gestion academica, en el que se incluye su información académica.</p>
     <p>Este sistema está a su disposición, esperamos que le permita accdeder de manera rápida y eficiente a la informacion que busca.</p>
     </div>
-    <div>
-        <div><a href="perfil.php">Ver mi perfil </a></div>
-        <div><a href="">Reportes</a></div>
-        <div><a href="">Ver mis cursos</a></div>
-        <div><a href="">Matrícula</a></div>
-        <div><a href="">Horario</a></div>
-        
+    <div class="container">
+        <div class="card">
+            <div class="circle">
+                <h2>Perfil</h2>
+            </div>
+            <div class="content">
+                <p>Ver,cambiar y 
+                    administrar tu 
+                    perfil</p>
+                <a href="perfil.php">entrar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="circle">
+                <h2>Reportes</h2>
+            </div>
+            <div class="content">
+                <p>Ver,cambiar y 
+                    administrar tu 
+                    perfil</p>
+                <a href="perfil.php">entrar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="circle">
+                <h2>Cursos</h2>
+            </div>
+            <div class="content">
+                <p>Ver,cambiar y 
+                    administrar tu 
+                    perfil</p>
+                <a href="perfil.php">entrar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="circle">
+                <h2>Matricula</h2>
+            </div>
+            <div class="content">
+                <p>Ver,cambiar y 
+                    administrar tu 
+                    perfil</p>
+                <a href="perfil.php">entrar</a>
+            </div>
+        </div>
+        <div class="card">
+            <div class="circle">
+                <h2>Horario</h2>
+            </div>
+            <div class="content">
+                <p>Ver,cambiar y 
+                    administrar tu 
+                    perfil</p>
+                <a href="perfil.php">entrar</a>
+            </div>
+        </div>
     </div>
 
     
